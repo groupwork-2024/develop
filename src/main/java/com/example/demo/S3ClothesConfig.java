619f5c355eb61d263ece1d8b5fae0dedc1cc3ada
akito <aki4.arapz@gmail.com>
2024-12-15 23:35:31 +0900

    s3セッティング

---
+++ src/main/java/com/example/demo/S3ClothesConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3ClothesConfig {
+    @Value("${aws.access-key}")
+    private String accessKey;
+
+    @Value("${aws.secret-key}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region)) // プロパティからリージョンを取得
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey) // プロパティからキーを取得
+                ))
+                .build();
+    }
+}
