6b1a42bbd68e64b461573d916c0cf93b9d466354
akito <aki4.arapz@gmail.com>
2024-12-17 13:38:47 +0900

    s3設定変更&アップロードメソッド変更

--- src/main/java/com/example/demo/damain/service/S3StorageService.java
+++ src/main/java/com/example/demo/damain/service/S3StorageService.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
+
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Service
 public class S3StorageService {
@@ -20,18 +22,23 @@ public class S3StorageService {
     private String bucketName;
 
     public String uploadFile(String folder, MultipartFile file) throws IOException {
-        // アップロードするS3内のファイルパス
-        String fileName = folder + "/" + file.getOriginalFilename(); // フォルダパスを含むファイル名
+
+        // フォルダ名が指定されていれば、"/"を末尾に追加する
+        String folderPath = folder != null && !folder.isEmpty() ? folder + "/" : "";
+
+        // ファイル名にUUIDを付与して一意にする
+        String uniqueFileName = folderPath + UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
 
         // PutObjectRequestを作成
         PutObjectRequest request = PutObjectRequest.builder()
                 .bucket(bucketName)
-                .key(fileName)
+                .key(uniqueFileName) // フォルダ名 + ファイル名
                 .build();
 
         // ファイル内容をRequestBodyに変換し、S3にアップロード
         s3Client.putObject(request, RequestBody.fromBytes(file.getBytes()));
+
         // アップロードされたファイルのURLを返す
-        return String.format("https://%s.s3.amazonaws.com/%s", bucketName, fileName);
+        return String.format("https://%s.s3.amazonaws.com/%s", bucketName, uniqueFileName);
     }
 }
