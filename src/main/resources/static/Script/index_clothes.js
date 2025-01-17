8158fd636764719d6f8ac869f8df8345fa42c662
natsuki-horiki <kd1343255@st.kobedenshi.ac.jp>
2025-01-17 23:42:07 +0900

    一覧画面にフレームカラー適用

--- src/main/resources/static/Script/index_clothes.js
+++ src/main/resources/static/Script/index_clothes.js
@@ -690,3 +690,16 @@ function stopCamera() {
   fileInput.style.display='flex';
   addPhotoButton.style.display='flex';
 }
+
+
+//画面の色（モード）を反映
+// 保存されたクラス名を取得してMypageに適用
+var savedClass = localStorage.getItem('colorClass');
+if (savedClass) {
+  //ログ
+  console.log('クラス取得できたよ', savedClass);
+
+  //ラベルに適用
+  const name = document.querySelector('.list_name');
+  name.classList.add(savedClass);
+}
