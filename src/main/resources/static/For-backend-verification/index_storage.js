536f30fcfc59eda6f65769225a4d04589ce93e49
natsuki-horiki <kd1343255@st.kobedenshi.ac.jp>
2025-01-20 00:17:43 +0900

    一覧画面のヘッダーにフレームカラー適用

--- src/main/resources/static/For-backend-verification/index_storage.js
+++ src/main/resources/static/For-backend-verification/index_storage.js
@@ -54,3 +54,16 @@ function ProcessingInstruction() {
     storage_type=storage_type.toLowerCase();
     window.location.href = `detail_${storage_type}.html?name=${name}`;
 }
+
+
+//画面の色（モード）を反映
+// 保存されたクラス名を取得してMypageに適用
+var savedClass = localStorage.getItem('colorClass');
+if (savedClass) {
+  //ログ
+  console.log('クラス取得できたよ', savedClass);
+
+  //ラベルに適用
+  const name = document.querySelector('.list_name');
+  name.classList.add(savedClass);
+}
